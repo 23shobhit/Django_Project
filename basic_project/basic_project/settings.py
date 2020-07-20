ab261ad69e32d105ed04aa1a45ee1995d47fd0da
23shobhit <51656679+23shobhit@users.noreply.github.com>
2020-07-21 00:09:25 +0530

    initial commit

--- basic_project/basic_project/settings.py
+++ basic_project/basic_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoapp-sj.herokuapp.com']
+ALLOWED_HOSTS = ['djangoblog-sj.herokuapp.com']
 
 
 # Application definition
